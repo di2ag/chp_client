a3c8d258a7e7059978b1e5fe07709a4159ac2a31
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2020-12-01 13:11:19 -0500

    updated client to include accurate versioning that can be grabbed as a module attribute

--- chp_client/__init__.py
+++ chp_client/__init__.py
@@ -5,7 +5,8 @@ Generic Client for Connections Hypothesis API.
 import types
 import copy
 
-from chp_client.client import ChpClient 
+from chp_client.client import ChpClient
+from chp_client._version import __version__
 
 # Function aliases common to all clients
 COMMON_ALIASES = {
